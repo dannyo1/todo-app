9654758aa7683ef87ea48e08aef508c35f6b8e45
dannyo1 <dannyo1@live.unc.edu>
2021-02-19 22:04:09 -0500

    giving up for now

--- src/data/markdown/app_background.js
+++ src/data/markdown/app_background.js
@@ -1,4 +1,5 @@
 const app_background = `##### An app allowing you to sort your life. 
-###### Built with React & Bootstrap!`;
+###### Built with React & Bootstrap!
+###### this doesn't work yet but i'm gonna keep at it l8r`;
 
 export default app_background;
