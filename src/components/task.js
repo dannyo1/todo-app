815a37cf9f0e2d32e31a3e766419e81beea7c269
dannyo1 <dannyo1@live.unc.edu>
2021-02-19 20:04:03 -0500

    first attempt

--- src/components/task.js
+++ src/components/task.js
@@ -1,11 +1,14 @@
 import React from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Task = ({ name }) => {
   return (
     <div>
       {/**
        * TODO: add an html or bootstrap element that uses the name variable to display an individual task
-       */}
+       */
+       <h1>{this.name}</h1>
+       }
     </div>
   );
 };
